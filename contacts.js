c6acc9a301d7d8464b9113b42ecbc6485d264bbe
Maxim <Tor968@yahoo.com>
2021-02-12 11:51:50 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,37 +1,38 @@
 import path from "path";
 import * as fs from "fs/promises";
 
-const contactsPath = path.resolve("/db/contacts", "contacts.json");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-fs.readFile(contactsPath)
-  .then((data) => console.log(data.toString()))
-  .catch((err) => console.log(err.message));
-// fs.readFile(contactsPath, "utf8", (err, data) => {
+// fs.readFile(contactsPath)
+//   .then((data) => console.log(data.toString()))
+//   .catch((err) => console.log(err.message));
+// // fs.readFile(contactsPath, "utf8", (err, data) => {
+// //   if (err) throw err;
+
+// //   console.log(data);
+// // });
+
+// fs.writeFile(contactsPath, "File Content", "utf8", (err) => {
 //   if (err) throw err;
 
-//   console.log(data);
+//   console.log("Done");
 // });
 
-fs.writeFile(contactsPath, "File Content", "utf8", (err) => {
-  if (err) throw err;
+module.exports.listContacts() = async function () {
+  const contacts = await fs.readFile(contactsPath, "utf8");
+  console.log("contacts=>", JSON.parse(contacts));
+};
 
-  console.log("Done");
-});
+// function getContactById(contactId) {
+//   // ...твой код
+// }
 
-function listContacts() {
-  // ...твой код
-}
+// function removeContact(contactId) {
+//   // ...твой код
+// }
 
-function getContactById(contactId) {
-  // ...твой код
-}
+// function addContact(name, email, phone) {
+//   // ...твой код
+// }
 
-function removeContact(contactId) {
-  // ...твой код
-}
-
-function addContact(name, email, phone) {
-  // ...твой код
-}
-
-module.exports = { listContacts, getContactById, removeContact, addContact };
+// module.exports = { listContacts, getContactById, removeContact, addContact };
