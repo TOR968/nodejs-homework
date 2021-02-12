692e6675182213031bf37600a8d62ffed93d5191
Maxim <Tor968@yahoo.com>
2021-02-12 10:54:08 +0200

    Update contacts.js

---
+++ contacts.js
@@ -0,0 +1,37 @@
+import path from "path";
+import * as fs from "fs/promises";
+
+const contactsPath = path.resolve("/db/contacts", "contacts.json");
+
+fs.readFile(contactsPath)
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err.message));
+// fs.readFile(contactsPath, "utf8", (err, data) => {
+//   if (err) throw err;
+
+//   console.log(data);
+// });
+
+fs.writeFile(contactsPath, "File Content", "utf8", (err) => {
+  if (err) throw err;
+
+  console.log("Done");
+});
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
