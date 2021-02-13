f870e96d9c336a48a6e02ea1385c18b9db1a9313
TOR968 <tor968@yahoo.com>
2021-02-13 22:20:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const contacts = require("./contacts");
-const argv = require("yargs").argv;
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('./contacts');
+const argv = require('yargs').argv;
 
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
-      contacts.listContacts();
-      // ...
+    case 'list':
+      listContacts();
       break;
 
-    // case "get":
-    //   // ... id
-    //   break;
+    case 'get':
+      getContactById(id);
+      break;
 
-    // case "add":
-    //   // ... name email phone
-    //   break;
+    case 'add':
+      addContact(name, email, phone);
+      break;
 
-    // case "remove":
-    //   // ... id
-    //   break;
+    case 'remove':
+      removeContact(id);
+      break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
